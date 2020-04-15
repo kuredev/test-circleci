a8727cbd1224e57bf8ffb99970a73cf478775866
Akira Kure <rapuntuleru2@gmail.com>
2020-04-15 12:11:49 +0900

    add

--- test2.rb
+++ test2.rb
@@ -4,12 +4,3 @@ puts 'hoge'
 
 puts "hoge"
 puts 'hoge'
-
-
-def raises_sometimes(value)
-  if value == 1
-    raise '1 is bad value!'
-  elif value == 2
-    raise '2 is bad value, too!'
-  end
-end
